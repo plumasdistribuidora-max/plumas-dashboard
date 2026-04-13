888adabcd3ee636aa8684fa2cea62f053201d719
plumasdistribuidora-max <plumasdistribuidora@gmail.com>
2026-04-13 16:48:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
+    with open(os.path.join(os.path.dirname(__file__), 'index.html'), 'r', encoding='utf-8') as f:
+        return f.read()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8080)
